7b92c4584b1ef85675d657b4c635e55fd5d469d1
David Johnson <djohnson.m@gmail.com>
2013-09-06 18:10:10 -0500

    added site, 404, api calls

--- app.py
+++ app.py
@@ -5,23 +5,22 @@ import os
 app = Flask(__name__)
 api = InstagramAPI(client_id='63b8fc9a82454cdfbf6e9899d4b7547e')
 
+#TODO: 
 #get tags working...
+#analytics
+#google ads
+#background worker with redis? mongo?
 
 @app.route('/')
 def home():
-    tags = api.media_popular(count=20)
+    tags = map(lambda x: x.name, api.tag_search("insta", 30)[0])
     return render_template('index.html', tags=tags)
     
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
-@app.after_request
-def add_header(response):
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
